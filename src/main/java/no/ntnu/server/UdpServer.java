8bac89d17b4187bdffcc10f1fedca2b7674f9481
emmanes <Emma.nesvik@hotmail.com>
2023-10-27 22:44:43 +0200

    Update UdpServer.java

--- src/main/java/no/ntnu/server/UdpServer.java
+++ src/main/java/no/ntnu/server/UdpServer.java
@@ -4,6 +4,9 @@ import java.io.IOException;
 import java.net.DatagramPacket;
 import java.net.DatagramSocket;
 
+/**
+ * A UDP server, which handles multiple clients.
+ */
 public class UdpServer {
 
   public static final int UDP_PORT = 12345;
@@ -18,8 +21,11 @@ public class UdpServer {
   }
 
   private void run() {
+
+    //If there is a connection, then the server is running.
     if (openListeningSocket()) {
       runningState = true;
+
       while (runningState) {
         DatagramPacket clientDatagram = receiveClientDatagram();
         if (clientDatagram != null) {
@@ -28,12 +34,20 @@ public class UdpServer {
         }
       }
     }
-    System.out.println("Server exiting...");
+    //TODO: add a more meaningful message that is our own.
+    System.out.println("Server exiting");
   }
 
+  /**
+   * DatagramPacket is a form of connectionless packet delivery service.
+   * @return
+   */
   private DatagramPacket receiveClientDatagram() {
-    byte[] receivedData = new byte[200];
-    DatagramPacket clientDatagram = new DatagramPacket(receivedData, receivedData.length);
+    byte[] dataReceived = new byte[400];
+    //TODO: Make this our own.
+
+    //constructor(byte[] buf, int length). (buf for buffer, holding incoming data).
+    DatagramPacket clientDatagram = new DatagramPacket(dataReceived, dataReceived.length);
     try {
       socket.receive(clientDatagram);
     } catch (IOException e) {
@@ -43,15 +57,24 @@ public class UdpServer {
     return clientDatagram;
   }
 
+  /**
+   * Opens up a socket that is listening for incoming connections.
+   *
+   * DatagramSocket is a socket for sending and receiving datagram packets.
+   * Socket is an endpoint of a two-way communication link
+   * between two programs running on the network.
+   * @return
+   */
   private boolean openListeningSocket() {
+    //TODO: Make this our own.
     boolean success = false;
     try {
       socket = new DatagramSocket(UDP_PORT);
-      System.out.println("Server listening on port " + UDP_PORT);
+      System.out.println("Server listening on port number: " + UDP_PORT);
       success = true;
     } catch (IOException e) {
-      System.err.println("Could not open a listening socket on port " + UDP_PORT
-          + ", reason: " + e.getMessage());
+      System.err.println("Error, could not open a listening socket on port " + UDP_PORT
+          + ", because: " + e.getMessage());
     }
     return success;
   }
