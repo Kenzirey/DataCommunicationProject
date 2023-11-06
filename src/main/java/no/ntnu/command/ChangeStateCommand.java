e3f43f3898901acc9e0fdf5320e790d1dfa8199f
RF <rf@bl2020.com>
2023-11-06 14:46:43 +0100

    dummy commands

---
+++ src/main/java/no/ntnu/command/ChangeStateCommand.java
@@ -0,0 +1,5 @@
+package no.ntnu.command;
+
+public class ChangeStateCommand {
+
+}
--- src/main/java/no/ntnu/command/TemperatureCommand.java
+++ src/main/java/no/ntnu/command/TemperatureCommand.java
@@ -1,9 +1,12 @@
 package no.ntnu.command;
-
+import no.ntnu.greenhouse.Sensor;
 public class TemperatureCommand implements Command {
 
     @Override
     public String execute(String[] args) {
+        // either get a sensor as a parameter or create one
+        // check for sensor type == temperature
+        // get value
         return "Temperature";
     }
 }
