ed2cbab98fb5c93faaa9bd075fe696dc6497f8e1
Tony Nhat Huy Pham <Tony.n.h.pham@hotmail.com>
2023-11-10 10:43:21 +0100

    change port :)

--- src/main/java/no/ntnu/server/Server.java
+++ src/main/java/no/ntnu/server/Server.java
@@ -17,7 +17,7 @@ public class Server extends Thread {
   //TODO: make a shutdown() command or "end"?
 
   //TODO: implement logic to allow adding multiple clients?
-  private static final int SERVER_PORT = 12345;
+  private static final int SERVER_PORT = 12346;
   private final DatagramSocket socket;
   private boolean running;
   private final byte[] buffer = new byte[1024];
