608f1b5f4c4723df4eebd452dd3bf22afb6db7ce
emmanes <Emma.nesvik@hotmail.com>
2023-11-13 14:03:10 +0100

    Work began on server refactoring.
    It is not complete yet, but working on the sockets and correct cohesion.

--- src/main/java/no/ntnu/controlpanel/UdpCommunicationChannel.java
+++ src/main/java/no/ntnu/controlpanel/UdpCommunicationChannel.java
@@ -1,50 +1,130 @@
 package no.ntnu.controlpanel;
 
-import no.ntnu.server.Server;
-
 import java.io.IOException;
-import java.net.*;
+import java.net.DatagramPacket;
+import java.net.DatagramSocket;
+import java.net.InetAddress;
+import java.net.SocketException;
+import no.ntnu.tools.Logger;
 
+/**
+ * Manages a communication channel using UDP protocol.
+ * Handles sending and receiving DatagramPackets to/from a server.
+ */
 public class UdpCommunicationChannel implements CommunicationChannel {
+
+  private final ControlPanelLogic logic;
   private DatagramSocket socket;
   private InetAddress serverAddress;
   private int serverPort;
-  private Server server;
 
-  public UdpCommunicationChannel(String serverAddress, int serverPort) throws UnknownHostException {
-    this.serverAddress = InetAddress.getByName(serverAddress);
-    this.serverPort = server.getServerPort();
-  }
-
-  @Override
-  public boolean open() {
+  /**
+   * Create a new UDP communication channel.
+   *
+   * @param logic central logic instance of a control panel node.
+   *
+   * @param serverIp the IP address of the server.
+   *
+   * @param serverPort the port number for the UDP server.
+   */
+  public UdpCommunicationChannel(ControlPanelLogic logic, String serverIp, int serverPort) {
+    this.logic = logic;
+    this.serverPort = serverPort;
     try {
-      socket = new DatagramSocket();
-      return true;
-    } catch (SocketException e) {
-      e.printStackTrace();
-      return false;
+      this.serverAddress = InetAddress.getByName(serverIp);
+
+      //System chooses available client port, so not specified here.
+      this.socket = new DatagramSocket();
+    } catch (IOException e) {
+      throw new RuntimeException("Error setting up UDP socket", e);
     }
   }
 
+  /**
+   * Receive a datagram packet.
+   *
+   * @return packet the received datagram packet.
+   *
+   * @throws IOException input/output exception.
+   */
+  public DatagramPacket receivePacket() throws IOException {
+    byte[] buffer = new byte[1024];
+    DatagramPacket packet = new DatagramPacket(buffer, buffer.length);
+    socket.receive(packet);
+    return packet;
+  }
+
+  /**
+   * Sends a command to change the state of an actuator via UDP.
+   *
+   * @param nodeId     ID of the node to which the actuator is attached
+   * @param actuatorId Node-wide unique ID of the actuator
+   * @param isOn       When true, actuator must be turned on; off when false.
+   */
   @Override
   public void sendActuatorChange(int nodeId, int actuatorId, boolean isOn) {
-    String message = String.format("CHANGE %d %d %b", nodeId, actuatorId, isOn);
-    byte[] buffer = message.getBytes();
-    DatagramPacket packet = new DatagramPacket(buffer, buffer.length, serverAddress, serverPort);
+    String message = "Node: " + nodeId + ", Actuator: " + actuatorId + ", State: " + (isOn ? "ON" : "OFF");
+    byte[] sendData = message.getBytes();
+    DatagramPacket sendPacket = new DatagramPacket(sendData, sendData.length, serverAddress, serverPort);
 
     try {
-      socket.send(packet);
+      socket.send(sendPacket);
+      Logger.info("Sent command to server: " + message);
     } catch (IOException e) {
-      e.printStackTrace();
+      System.err.println("Error sending UDP packet" + e.getMessage());
     }
   }
 
-  // You may also want to add a method to close the socket when done
-  public void close() {
+  /**
+   * Opens the UDP socket if not already open.
+   *
+   * @return true if socket is opened, false otherwise.
+   */
+  @Override
+  public boolean open() {
+    if (socket == null || socket.isClosed()) {
+      try {
+        socket = new DatagramSocket();
+        return true;
+      } catch (SocketException e) {
+        System.err.println("Error opening UDP socket" + e.getMessage());
+        return false;
+      }
+    }
+    return true;
+  }
+
+  /**
+   * Closes the UDP socket.
+   */
+  public void closeSocket() {
     if (socket != null && !socket.isClosed()) {
       socket.close();
     }
   }
-}
 
+  /**
+   * Returns the UDP socket.
+   *
+   * @return socket returns the UDP socket.
+   */
+  public DatagramSocket getSocket() {
+    return this.socket;
+  }
+
+  /**
+   * Sends a DatagramPacket.
+   *
+   * @param packet the DatagramPacket being sent.
+   *
+   * @throws IOException input/output exception.
+   */
+  public void sendPacket(DatagramPacket packet) throws IOException {
+    if (socket != null && !socket.isClosed()) {
+      socket.send(packet);
+    } else {
+      throw new SocketException("Socket is closed or not initialized");
+    }
+  }
+  //TODO: separate thread or listener for the sending and receiving?
+}
--- src/main/java/no/ntnu/server/Server.java
+++ src/main/java/no/ntnu/server/Server.java
@@ -1,11 +1,10 @@
 package no.ntnu.server;
 
-import no.ntnu.greenhouse.GreenhouseSimulator;
+import no.ntnu.controlpanel.ControlPanelLogic;
+import no.ntnu.controlpanel.UdpCommunicationChannel;
 
 import java.io.IOException;
 import java.net.DatagramPacket;
-import java.net.DatagramSocket;
-import java.net.SocketException;
 import java.nio.charset.StandardCharsets;
 
 /**
@@ -13,39 +12,41 @@ import java.nio.charset.StandardCharsets;
  * The server will process the incoming packets,
  * and send a response back to the client.
  */
-public class Server extends Thread {
+public class Server extends Thread implements ServerMessageListener {
 
   //TODO: implement logic to allow adding multiple clients?
   private static final int SERVER_PORT = 12346;
-  private final DatagramSocket socket;
+  private final UdpCommunicationChannel udpChannel;
+  private ControlPanelLogic controlPanelLogic;
   private boolean running;
   private final byte[] buffer = new byte[1024];
   private static final String serverStopping = "Server is shutting down..";
-  private final GreenhouseSimulator greenhouseSimulator;
-
-  //TODO: Remove main method when GreenhouseSimulator starts it instead.
+  private ServerMessageListener messageListener;
   public static void main(String[] args) {
-    final Server server = new Server(SERVER_PORT, new GreenhouseSimulator(false));
+    // Simple implementation of ServerMessageListener for testing
+    ServerMessageListener messageListener = new ServerMessageListener() {
+      @Override
+      public void onMessageReceived(String message) {
+        System.out.println("Received message: " + message);
+      }
+    };
 
-    //run() is invoked when you call start on a server object.
+    // Create the server instance with the test listener
+    Server server = new Server(12346, messageListener);
     server.start();
-    System.out.println("UDP Server is up and running on port " + SERVER_PORT);
 
   }
 
   /**
    * Constructs a new server instance,
    * which opens a DatagramSocket to listen for incoming packets.
-   * @param port the port number to listen on for packets.
    */
-  public Server(int port, GreenhouseSimulator greenhouseSimulator) {
-    this.greenhouseSimulator = greenhouseSimulator;
-    //This starts up when start() method is called.
-    try {
-      socket = new DatagramSocket(port);
-    } catch (SocketException e) {
-      throw new RuntimeException(e);
-    }
+  public Server(int SERVER_PORT, ServerMessageListener messageListener) {
+    this.controlPanelLogic = new ControlPanelLogic();
+    this.messageListener = messageListener;
+    this.udpChannel = new UdpCommunicationChannel(controlPanelLogic, "localhost", SERVER_PORT);
+    this.controlPanelLogic.setCommunicationChannel(udpChannel);
+    this.udpChannel.open();
   }
 
   /**
@@ -55,26 +56,20 @@ public class Server extends Thread {
   public void run() {
     running = true;
 
-    //This method is called when you use start() on a server object.
     while (running) {
-      //Packet to receive data into.
-      DatagramPacket packet = new DatagramPacket(buffer, buffer.length);
       try {
-        //Receive packet from client.
-        socket.receive(packet);
-        //Converts byte data in packet to a String (via default char encoding).
-        String received = new String(packet.getData(), 0, packet.getLength());
-        //
-        handleRequest(received, packet);
+        DatagramPacket packet = udpChannel.receivePacket();
+
+        handleRequest(new String(packet.getData(), 0, packet.getLength(), StandardCharsets.UTF_8), packet);
       } catch (IOException e) {
-        //If socket is closed and server is no longer running, break the out of the loop.
-        if (socket.isClosed() && !running) {
+        if (!running) {
           break;
         }
         System.err.println("Error processing packet: " + e.getMessage());
       }
     }
-    socket.close();
+
+    udpChannel.closeSocket();
   }
 
 
@@ -88,20 +83,22 @@ public class Server extends Thread {
    * @throws IOException input/output exception .
    */
   private void handleRequest(String message, DatagramPacket packet) throws IOException {
-    if ("end".equals(message)) {
+    if ("end".equals(message.trim())) {
       byte[] responseData = serverStopping.getBytes(StandardCharsets.UTF_8);
       DatagramPacket responseDatagram = new DatagramPacket(responseData, responseData.length,
               packet.getAddress(), packet.getPort());
 
-      //Sends shutdown message back to client.
-      socket.send(responseDatagram);
+      udpChannel.sendPacket(responseDatagram);
       running = false;
       return;
     }
     //Handles the command received in the packet from client.
-    DatagramHandler handler = new DatagramHandler(socket, packet);
+    DatagramHandler handler = new DatagramHandler(udpChannel.getSocket(), packet);
     handler.run();
-    //TODO: greenhouseSimulator.updateNodeWithCommand(message);
+
+    if (messageListener!= null) {
+      messageListener.onMessageReceived(message);
+    }
   }
 
   /**
@@ -109,11 +106,16 @@ public class Server extends Thread {
    */
   public void shutdown() {
     running = false;
-    socket.close();
+    udpChannel.closeSocket();
     System.out.println(serverStopping);
   }
 
   public int getServerPort() {
     return SERVER_PORT;
+  }
+
+  @Override
+  public void onMessageReceived(String message) {
+   //TODO: what to do here
   }
 }
