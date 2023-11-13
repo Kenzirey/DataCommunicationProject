91e322e20062cab47e3d465280dd138deab2bbd8
RF <rf@bl2020.com>
2023-11-13 12:00:42 +0100

    bit more of error handling

--- src/main/java/no/ntnu/command/ChangeStateCommand.java
+++ src/main/java/no/ntnu/command/ChangeStateCommand.java
@@ -11,7 +11,7 @@ import java.util.Objects;
 public class ChangeStateCommand implements Command {
     /*
     Command Example
-    changeState, State (on, off), Type, ID
+    changeState, State (on, off), NodeID, ActuatorID
     changeState on fan 3
      */
     @Override
@@ -22,30 +22,37 @@ public class ChangeStateCommand implements Command {
            Change State
            Return Succesful Change
          */
-        // either creating the channel here or give it as parameter
         GreenhouseSimulator gs = new GreenhouseSimulator(true);
         Map<Integer, SensorActuatorNode> mapNodes = gs.getNodes();
 
-        int actuatorId = 0;
+        int nodeID = 0;
+        int actuatorID = 0;
         boolean checkSuccesful = false;
+        boolean nodeIDWorked = false;
         String isOn = args[1];
         boolean boolIsOn = Objects.equals(isOn, "on");
-        String type = args[2];
         try {
-            actuatorId = Integer.parseInt(args[3]);
+            nodeID = Integer.parseInt(args[3]);
+            nodeIDWorked = true;
+            actuatorID = Integer.parseInt(args[3]);
             checkSuccesful = true;
 
         } catch (java.lang.NumberFormatException e) {
-            System.out.printf("Identifier ID: %s is not a number", args[3]);
+            if (nodeIDWorked) System.out.printf("Identifier Actuator-ID: %s is not a number", args[3]);
+            else System.out.printf("Identifier Node-ID: %s is not a number", args[2]);
         }
         if (checkSuccesful) {
-            Actuator act = new Actuator(type, actuatorId);
-            if (act.isOn() == boolIsOn) {
-                System.out.printf(" Actuator with  ID: %s is already on the state %s", args[3], args[1]);
-            } else {
-                act.toggle();
-                System.out.printf(" Actuator with  ID: %s is now turned %s!", args[3], args[1]);
+            if(mapNodes.containsKey(nodeID)){
+                SensorActuatorNode node = mapNodes.get(nodeID);
+                Actuator act = node.getActuators().get(actuatorID);
+                if (act.isOn() == boolIsOn) {
+                    System.out.printf(" Actuator with ID %s, on Node %s, is already turned %s", args[3], args[2], args[1]);
+                } else {
+                    act.toggle();
+                    System.out.printf(" Actuator with ID %s, on Node %s, is now turned %s!", args[3], args[2], args[1]);
+                }
             }
+
         }
 
 
