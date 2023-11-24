0169f2739f0ceb15c4d09213ebda0eeac605c663
RF <rf@bl2020.com>
2023-11-24 11:48:46 +0100

    simple version live commands

--- src/main/java/no/ntnu/server/MainClientTester.java
+++ src/main/java/no/ntnu/server/MainClientTester.java
@@ -2,10 +2,14 @@ package no.ntnu.server;
 
 import no.ntnu.greenhouse.GreenhouseSimulator;
 
+import java.io.IOException;
+import java.util.Scanner;
 import java.net.DatagramPacket;
 import java.net.DatagramSocket;
 import java.net.InetAddress;
 import java.nio.charset.StandardCharsets;
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
 
 /**
  * A dummy client for testing various commands.
@@ -31,30 +35,44 @@ public class MainClientTester {
       }
     }
 
-    sendCommandToServer("time");
+    // using scanner to get keyboard input
+    Scanner scanner = new Scanner(System.in);
 
-    sendCommandToServer("name");
+    // loop which is permanently true til exit
+    while (true) {
+      System.out.print("Enter command (or 'exit' to quit): ");
+      String command = scanner.nextLine();
+
+      if ("exit".equalsIgnoreCase(command)) {
+        System.out.println("exited the scanner");
+        break;
+      }
+      System.out.println("test");
+      sendCommandToServer(command);
+      System.out.println(command + "sent");
+    }
+
+    scanner.close();
   }
 
+    private static void sendCommandToServer (String command){
+      try (DatagramSocket socket = new DatagramSocket()) {
+        byte[] buffer;
 
+        buffer = command.getBytes(StandardCharsets.UTF_8);
+        DatagramPacket packet = new DatagramPacket(buffer, buffer.length, InetAddress.getByName(SERVER_ADDRESS), server.getServerPort());
+        socket.send(packet);
 
-  private static void sendCommandToServer(String command) {
-    try (DatagramSocket socket = new DatagramSocket()) {
-      byte[] buffer;
+        // Response
+        byte[] receiveBuffer = new byte[1024];
+        DatagramPacket receivePacket = new DatagramPacket(receiveBuffer, receiveBuffer.length);
+        socket.receive(receivePacket);
 
-      buffer = command.getBytes(StandardCharsets.UTF_8);
-      DatagramPacket packet = new DatagramPacket(buffer, buffer.length, InetAddress.getByName(SERVER_ADDRESS), server.getServerPort());
-      socket.send(packet);
-
-      // Response
-      byte[] receiveBuffer = new byte[1024];
-      DatagramPacket receivePacket = new DatagramPacket(receiveBuffer, receiveBuffer.length);
-      socket.receive(receivePacket);
-
-      String received = new String(receivePacket.getData(), 0, receivePacket.getLength(), StandardCharsets.UTF_8);
-      System.out.println("Received: " + received);
-    } catch (Exception e) {
-      e.printStackTrace();
+        String received = new String(receivePacket.getData(), 0, receivePacket.getLength(), StandardCharsets.UTF_8);
+        System.out.println("Received: " + received);
+      } catch (Exception e) {
+        e.printStackTrace();
+      }
     }
   }
-}
+
