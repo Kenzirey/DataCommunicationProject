f9e2eb02e6840cd4ebce2481ba91d50668afceac
emmanes <Emma.nesvik@hotmail.com>
2023-11-25 17:48:23 +0100

    Fixed bugs with testing

--- src/test/UdpCommServerTest.java
+++ src/test/UdpCommServerTest.java
@@ -86,8 +86,6 @@ class UdpCommServerTest {
    */
   @Test
   void testShutdown() throws InterruptedException {
-    server.start();
-
     //Synchronized to make sure the server is running before the assertion is made.
     synchronized(server) {
       server.wait(5);
